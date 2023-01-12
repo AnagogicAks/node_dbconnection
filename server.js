c39f05c36ca76e83589754662c4ae9dde7bc5cb0
AnagogicAks <shrivastavaajay19@gmail.com>
2023-01-12 10:40:01 +0530

    msg

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const mysql=require("mysql");
 const express=require("express");
 const path = require("path");
 const app=express();
+var bodyparser=require("body-parser");
+//this line will separate parts of url and gives the 
+//data in query object
+//extended:false will use querystring module to parse the data
+app.use(bodyparser.urlencoded({extended:false}));
+
+
 var con=mysql.createConnection({
     host:"localhost",
     user:"root",
-    port:3307,
+    port:3306,
     password:"root123",
     database:"mydatabase" 
 });
@@ -15,19 +22,32 @@ con.connect(function(err){
     
 });
 
+// con.query("create database mydatabase",function(err,result){
+//     if(err) throw err;
+//     else 
+//     console.log(result);
+// });
+
+// con.query("create table personaldata(id int primary key, name varchar(20),address varchar(20))",function(err,result){
+//     if(err) throw err;
+//     else 
+//     console.log(result);
+// });
+
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "index.html"));
-})
+});
+
 
 // app.get("/page", (req, res)=> {
 //     res.sendFile(path.join(__dirname, "page.html"));
 // })
 
 app.post("/page", (req, res) => {
-   var name =  req.query.name;
-   var add = req.query.address;
-
-   con.query("insert into personaldata values (4, '?', '?')",[ id,name, add ], function(err, result) {
+   var name =req.body.name;
+   var add = req.body.address;
+    var q = "insert into personaldata values (7, '"+name+"', '"+add+"')";
+   con.query(q, function(err, result) {
         if(err){
             console.log(err);
         }
@@ -49,11 +69,7 @@ console.log("port:7070");
 //     else 
 //     console.log(result);
 // } )
-// con.query("create table personaldata(id int primary key, name varchar(20),address varchar(20))",function(err,result){
-//     if(err) throw err;
-//     else 
-//     console.log(result);
-// } )
+
 // con.query("insert into personaldata (id,name,address) values (1,'pratiksha','mumbai')",function(err,result){
 //     if(err) throw err;
 //     else 
